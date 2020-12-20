2f87abf05b661a1be9f291ba5b13200b5585a0a2
AlexandreZT <a.zitounitinlot@gmail.com>
2020-12-20 16:39:04 +0000

    superuser added

--- projectname/projectname/settings.py
+++ projectname/projectname/settings.py
@@ -10,9 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-# superusername : strartuper
-# superuserpass : azqswx784512-+
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
