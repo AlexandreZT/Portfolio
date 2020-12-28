619c07791995d1e2c7a2f2a70d6a4c4621a344b0
DevAlexandre <a.zitounitinlot@gmail.com>
2020-12-28 17:08:46 +0100

    updates

--- projectname/projectname/urls.py
+++ projectname/projectname/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# from appname.views import welcome # specific function
-from appname import views # every functions -> views.welcome
+import appname.views 
+# from appname import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('welcome/', views.welcome),
-    path('', views.bienvenu)
+    path('', appname.views.bienvenu, name='accueil'),
+    # path('', views.bienvenu, name='accueil'),
 ] 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
